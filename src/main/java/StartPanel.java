f83ddb3b97d23dcf13c29d0b5c7dd4dc918c760f
HappyKotek <kosma.sosnowski@gmail.com>
2022-04-19 21:40:01 +0200

    Added save progress and restore progress.

--- src/main/java/StartPanel.java
+++ src/main/java/StartPanel.java
@@ -2,15 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class StartPanel extends JFrame implements ActionListener {
     ClickerPanel clickerPanel;
+    JButton restoreButton;
     JButton gameButton;
-    JButton save;
+    JButton saveButton;
     JPanel startPanel;
 
     StartPanel(){
-        save = new JButton("Save progress");
+        restoreButton = new JButton("Restore progress");
+        restoreButton.addActionListener(this);
+        saveButton = new JButton("Save progress");
+        saveButton.addActionListener(this);
         gameButton = new JButton("START");
         clickerPanel = new ClickerPanel();
         startPanel = new JPanel();
@@ -19,7 +24,6 @@ public class StartPanel extends JFrame implements ActionListener {
         gameButton.addActionListener(this);
         gameButton.setFocusable(false);
         startPanel.add(gameButton);
-
 
         this.setTitle("CLICKER");
         this.setSize(700,700);
@@ -35,17 +39,45 @@ public class StartPanel extends JFrame implements ActionListener {
         if (e.getSource() == gameButton){
                 changeContent();
         }
-        if (e.getSource() == save){
-            
+        if (e.getSource() == saveButton) {
+            try {
+                saveProgress();
+            } catch (IOException ex) {
+                System.out.println("Path not exist");
+            }
         }
+        if (e.getSource() == restoreButton){
+            try {
+                readProgress();
+            } catch (IOException ex) {
+                System.out.println("File not exist");
+            }
+        }
+
     }
 
     public void changeContent() {
         startPanel.remove(gameButton);
         startPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        startPanel.add(save);
+        startPanel.add(saveButton);
+        startPanel.add(restoreButton);
         this.add(clickerPanel);
         this.revalidate();
+    }
 
+    private void saveProgress() throws IOException {
+        File file = new File("src\\main\\java\\save_logs\\save");
+        FileWriter writer = new FileWriter(file);
+        writer.write("" + clickerPanel.score);
+        writer.close();
+        this.revalidate();
+    }
+
+    private void readProgress() throws IOException {
+        File file = new File("src\\main\\java\\save_logs\\save");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String data = reader.readLine();
+        reader.close();
+        clickerPanel.score = Integer.parseInt(data);
     }
 }
